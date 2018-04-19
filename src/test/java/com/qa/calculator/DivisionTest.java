5a49efae1ceda2a04a82b70b4b4925d18b710533
Godwin <Godwinadeleke@gmail.com>
2018-04-19 11:16:37 +0100

    Division Method With ZeroTest

--- src/test/java/com/qa/calculator/DivisionTest.java
+++ src/test/java/com/qa/calculator/DivisionTest.java
@@ -7,11 +7,28 @@ public class DivisionTest {
 	private Division divider;
 
 	@Test
-	public void test() {
+	public void DivideTest() {
 		divider = new Division();
 		int expectedValue = 5;
-		int actualValue =Division.divider(10,2);
-		Assert.assertEquals(expectedValue, actualValue);
+		int actualValue = Division.divider(10,2);
+			Assert.assertEquals(expectedValue, actualValue);
+		
+		}
+		
+	
+	
+	@Test
+	public void ZeroTest() {
+		divider = new Division();
+		String expectedValue = "Error";
+		int actualValue;
+		try {
+			actualValue = Division.divider(10, 0);
+			Assert.assertEquals(expectedValue, actualValue);
+		} catch (ArithmeticException e) {
+			System.out.println("error division by 0");
+		}
+		
 	}
 
 }
